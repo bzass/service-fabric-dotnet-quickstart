34de1f56d63bf1dd4e707c5793a2802007cfd228
Sławomir Kwasiborski <Slawomir.Kwasiborski@internetium.pl>
2018-11-14 16:11:56 +0000

    Create runchaos.ps1

---
+++ runchaos.ps1
@@ -0,0 +1,61 @@
+$timeToRunMinute = 30
+
+# The maximum amount of time to wait for all cluster entities to become stable and healthy. 
+# Chaos executes in iterations and at the start of each iteration it validates the health of cluster entities. 
+# During validation if a cluster entity is not stable and healthy within MaxClusterStabilizationTimeoutInSeconds, Chaos generates a validation failed event.
+$maxClusterStabilizationTimeSecs = 30
+
+# MaxConcurrentFaults is the maximum number of concurrent faults induced per iteration. 
+# Chaos executes in iterations and two consecutive iterations are separated by a validation phase. 
+# The higher the concurrency, the more aggressive the injection of faults -- inducing more complex series of states to uncover bugs. 
+# The recommendation is to start with a value of 2 or 3 and to exercise caution while moving up.
+$maxConcurrentFaults = 3
+
+# Time-separation (in seconds) between two consecutive iterations of Chaos. The larger the value, the lower the fault injection rate.
+$waitTimeBetweenIterationsSec = 10
+
+# Wait time (in seconds) between consecutive faults within a single iteration. 
+# The larger the value, the lower the overlapping between faults and the simpler the sequence of state transitions that the cluster goes through. 
+# The recommendation is to start with a value between 1 and 5 and exercise caution while moving up.
+$waitTimeBetweenFaultsSec = 0
+
+# Passed-in cluster health policy is used to validate health of the cluster in between Chaos iterations. 
+$clusterHealthPolicy = new-object -TypeName System.Fabric.Health.ClusterHealthPolicy
+$clusterHealthPolicy.MaxPercentUnhealthyNodes = 100
+$clusterHealthPolicy.MaxPercentUnhealthyApplications = 100
+$clusterHealthPolicy.ConsiderWarningAsError = $False
+
+
+$events = @{}
+$now = [System.DateTime]::UtcNow
+
+Start-ServiceFabricChaos -TimeToRunMinute $timeToRunMinute -MaxConcurrentFaults $maxConcurrentFaults -MaxClusterStabilizationTimeoutSec $maxClusterStabilizationTimeSecs -EnableMoveReplicaFaults -WaitTimeBetweenIterationsSec $waitTimeBetweenIterationsSec -WaitTimeBetweenFaultsSec $waitTimeBetweenFaultsSec -ClusterHealthPolicy $clusterHealthPolicy
+
+while($true)
+{
+    $report = Get-ServiceFabricChaosReport -StartTimeUtc $now -EndTimeUtc ([System.DateTime]::MaxValue)
+
+    foreach ($e in $report.History) {
+
+        if(-Not ($events.Contains($e.TimeStampUtc.Ticks)))
+        {
+            $events.Add($e.TimeStampUtc.Ticks, $e)
+            if($e -is [System.Fabric.Chaos.DataStructures.ValidationFailedEvent])
+            {
+                Write-Host -BackgroundColor White -ForegroundColor Red $e
+            }
+            else
+            {
+                Write-Host $e
+                # When Chaos stops, a StoppedEvent is created.
+                # If a StoppedEvent is found, exit the loop.
+                if($e -is [System.Fabric.Chaos.DataStructures.StoppedEvent])
+                {
+                    return
+                }
+            }
+        }
+    }
+
+    Start-Sleep -Seconds 1
+}
